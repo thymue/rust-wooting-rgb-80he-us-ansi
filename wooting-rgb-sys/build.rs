a82726bd5baa77659a4f87d16ad00e2b42cf3a21
Shayne Hartford <shaybox@shaybox.com>
2024-04-05 04:17:37 -0400

    0.3.2 - Fix Linux

--- wooting-rgb-sys/build.rs
+++ wooting-rgb-sys/build.rs
@@ -7,13 +7,6 @@ use std::{
 use pkg_config::find_library;
 
 fn main() {
-    // If enabled, attempt to find `wooting-rgb-sdk` via pkg-config. Otherwise, we'll
-    // need to build the SDK.
-    let use_pkg_config = env::var("WOOTING_RGB_SDK_SHARED").is_ok();
-    if use_pkg_config && find_library("wooting-rgb-sdk").is_ok() {
-        return;
-    }
-
     // Clone submodules if that hasn't already happened.
     if !Path::new("vendor/.git").exists() || !Path::new("vendor/hidapi/.git").exists() {
         let _ = Command::new("git")
@@ -41,37 +34,9 @@ fn main() {
         .include("vendor/hidapi/hidapi");
 
     if target.contains("linux") {
-        let use_pkg_config_for_hidapi = env::var("WOOTING_RGB_SDK_HIDAPI_SHARED").is_ok();
-        let use_hidraw_for_hidapi = env::var("WOOTING_RGB_SDK_HIDAPI_HIDRAW").is_ok();
-        match (use_pkg_config_for_hidapi, use_hidraw_for_hidapi) {
-            (true, false) => {
-                let lib = find_library("hidapi-libusb").expect("Unable to find hidapi-libusb");
-                for path in lib.include_paths {
-                    cfg.include(path.to_str().unwrap());
-                }
-            }
-            (true, true) => {
-                let lib = find_library("hidapi-hidraw").expect("Unable to find hidapi-hidraw");
-                for path in lib.include_paths {
-                    cfg.include(path.to_str().unwrap());
-                }
-            }
-            (false, false) => {
-                let libusb = find_library("libusb-1.0").expect("Unable to find libusb-1.0");
-                for path in libusb.include_paths {
-                    cfg.include(path.to_str().unwrap());
-                }
-
-                cfg.file("vendor/hidapi/libusb/hid.c");
-            }
-            (false, true) => {
-                let libudev = find_library("libudev").expect("Unable to find libudev");
-                for path in libudev.include_paths {
-                    cfg.include(path.to_str().unwrap());
-                }
-
-                cfg.file("vendor/hidapi/linux/hid.c");
-            }
+        let lib = find_library("hidapi-hidraw").expect("Unable to find hidapi-hidraw");
+        for path in lib.include_paths {
+            cfg.include(path.to_str().unwrap());
         }
     } else if target.contains("windows") {
         cfg.file("vendor/hidapi/windows/hid.c");
